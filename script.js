020349740edd3b0b70d9529abed10bb86657281c
Suraj Vaidyanathan <suraj.vaidyanathan@gmail.com>
2025-11-30 16:45:22 +0530

    Added grid of 16x16 divs

---
+++ script.js
@@ -0,0 +1,17 @@
+function createGrid(num){
+    for(let i = 0; i < num; i++){
+        let divRow = document.createElement("div");
+        divRow.classList.add("grid-row");
+        const divContainer = document.getElementsByClassName("grid-container")[0];
+        divContainer.appendChild(divRow);
+        for(let j = 0; j < num; j++){
+            let divNode = document.createElement("div");
+            divNode.classList.add("grid-element");
+            divNode.style.width = `${720 / num}px`;
+            divNode.style.height = `${720 / num}px`;
+            divRow.appendChild(divNode);
+        }
+    }
+}
+
+createGrid(16);
