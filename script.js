d1ebc2b884581915860da5f413c4f8905bf2d1b6
Suraj Vaidyanathan <suraj.vaidyanathan@gmail.com>
2025-11-30 17:12:48 +0530

    Added erase and reset grid buttons with script

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+const divContainer = document.getElementsByClassName("grid-container")[0];
+
 function createGrid(num){
     for(let i = 0; i < num; i++){
         let divRow = document.createElement("div");
         divRow.classList.add("grid-row");
-        const divContainer = document.getElementsByClassName("grid-container")[0];
         divContainer.appendChild(divRow);
         for(let j = 0; j < num; j++){
             let divNode = document.createElement("div");
             divNode.classList.add("grid-element");
-            divNode.style.width = `${720 / num}px`;
-            divNode.style.height = `${720 / num}px`;
+            divNode.style.width = `${480 / num}px`;
+            divNode.style.height = `${480 / num}px`;
             function changeColor(){
                 divNode.style.backgroundColor = "black";
             }
@@ -18,4 +19,29 @@ function createGrid(num){
     }
 }
 
+function resetGrid(num){
+    divContainer.innerHTML = '';
+    createGrid(num);
+}
+
 createGrid(16);
+
+const resetButton = document.querySelector(".reset");
+const gridSizeInput = document.querySelector(".grid-size");
+const eraseButton = document.querySelector(".erase");
+
+resetButton.addEventListener("click", () => {
+    const gridSize = parseInt(gridSizeInput.value);
+    if (!isNaN(gridSize) && gridSize > 0) {
+        resetGrid(gridSize);
+    } else {
+        alert("Please enter a valid number greater than 0.");
+    }
+});
+
+eraseButton.addEventListener("click", () => {
+    const gridElements = document.querySelectorAll(".grid-element");
+    gridElements.forEach(element => {
+        element.style.backgroundColor = 'grey';
+    })
+})
